9edc91c534903d9286b5742da22f850be79b5c72
sunnyvia <oktavia.anggarani.irawan@gmail.com>
2025-12-21 20:12:44 +0700

    Revert "Revert "Add shop, cart, wishlist, and coupon features""

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -309,11 +309,11 @@
             <li class="navigation__item">
               <a href="{{route('home.index')}}" class="navigation__link">Home</a>
             </li>
-            <li class="navigation__item">
-              <a href="shop.html" class="navigation__link">Shop</a>
+            <li class="navigation__item">              
+              <a href="{{ route('home.index')}}" class="navigation__link">Shop</a>
             </li>
             <li class="navigation__item">
-              <a href="cart.html" class="navigation__link">Cart</a>
+              <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
             </li>
             <li class="navigation__item">
               <a href="about.html" class="navigation__link">About</a>
@@ -398,10 +398,10 @@
               <a href="{{route('home.index')}}" class="navigation__link">Home</a>
             </li>
             <li class="navigation__item">
-              <a href="shop.html" class="navigation__link">Shop</a>
+              <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
             </li>
             <li class="navigation__item">
-              <a href="cart.html" class="navigation__link">Cart</a>
+              <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
             </li>
             <li class="navigation__item">
               <a href="about.html" class="navigation__link">About</a>
@@ -480,18 +480,23 @@
           </div>
           @endguest
 
-          <a href="wishlist.html" class="header-tools__item">
+          <a href="{{ route('wishlist.index') }}" class="header-tools__item header-tools__cart">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <use href="#icon_heart" />
             </svg>
+            @if(Cart::instance('wishlist')->content()->count() > 0)
+            <span class="cart-amount d-block position-absolute js-cart-items-count">{{ Cart::instance('wishlist')->content()->count() }}</span>
+            @endif
           </a>
 
-          <a href="cart.html" class="header-tools__item header-tools__cart">
+          <a href="{{ route('cart.index') }}" class="header-tools__item header-tools__cart">
             <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
               xmlns="http://www.w3.org/2000/svg">
               <use href="#icon_cart" />
             </svg>
-            <span class="cart-amount d-block position-absolute js-cart-items-count">3</span>
+            @if(Cart::instance('cart')->content()->count() > 0)
+            <span class="cart-amount d-block position-absolute js-cart-items-count">{{ Cart::instance('cart')->content()->count() }}</span>
+            @endif
           </a>
         </div>
       </div>
