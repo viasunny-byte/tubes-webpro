61106d065361c71138fece37130c4e5bc9522dba
heyhi-me <vanessafridanighta@student.telkomuniversity.ac.id>
2025-12-31 13:18:23 +0700

    Setup Scramble & expose API endpoints (shop, cart, wishlist, checkout, admin)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Providers;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Support\Facades\Route;
+
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +26,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         Scramble::routes(function () {
+        $allowedPrefixes = [
+            'shop',
+            'cart',
+            'wishlist',
+            'checkout',
+            'admin',
+        ];
+
+        return collect(Route::getRoutes())->filter(function ($route) use ($allowedPrefixes) {
+            $uri = $route->uri();   // contoh: "shop/{product_slug}"
+
+            foreach ($allowedPrefixes as $prefix) {
+                if (str_starts_with($uri, $prefix)) {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+    });
     }
 }
